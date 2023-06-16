b692fd3a13e9846ccc9b53e6db3f8bf7ef6fdb82
GZZ <gzz_2000@126.com>
2023-06-16 22:28:22 +0800

    fix scattered multibit on gate vcd

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,75 @@
 //! See the binaries for example usage.
 
 use indexmap::IndexMap;
-use std::collections::HashMap;
 use compact_str::CompactString;
 use serde::{ Serialize, Deserialize };
-use vcd_ng::{ Parser, FastFlow };
+use vcd_ng::{ Parser, FastFlow, ReferenceIndex, Var, ScopeItem, FastFlowToken, FFValueChange };
 use std::fs::File;
 use std::io::{ self, BufReader };
+use std::hash::{ Hash, Hasher };
+use std::borrow::Borrow;
+
+/// A general hier name with index, used as hashing.
+trait HierNameIdx {
+    fn hier(&self) -> &[CompactString];
+    fn idx(&self) -> Option<i32>;
+}
+
+impl Hash for dyn HierNameIdx + '_ {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.hier().hash(state);
+        self.idx().hash(state);
+    }
+}
+
+impl PartialEq for dyn HierNameIdx + '_ {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.idx() == other.idx() && self.hier() == other.hier()
+    }
+}
+
+impl Eq for dyn HierNameIdx + '_ {}
+
+impl HierNameIdx for (Vec<CompactString>, Option<i32>) {
+    #[inline]
+    fn hier(&self) -> &[CompactString] {
+        &self.0
+    }
+    
+    #[inline]
+    fn idx(&self) -> Option<i32> {
+        self.1
+    }
+}
+
+impl<'i> Borrow<dyn HierNameIdx + 'i> for (Vec<CompactString>, Option<i32>) {
+    #[inline]
+    fn borrow(&self) -> &(dyn HierNameIdx + 'i) {
+        self
+    }
+}
+
+impl HierNameIdx for (&Vec<CompactString>, Option<i32>) {
+    #[inline]
+    fn hier(&self) -> &[CompactString] {
+        &self.0
+    }
+    
+    #[inline]
+    fn idx(&self) -> Option<i32> {
+        self.1
+    }
+}
 
 /// The hash database.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct HashDB {
-    /// Net name to start indices and widths into `hashes` vector.
-    pub name2id: IndexMap<Vec<CompactString>, (usize, usize)>,
+    /// Net name to index in `hashes` vector.
+    ///
+    /// Vector bits will be mapped separately like
+    /// (xxx, 0) -> (start, 1), regardless of the declaration.
+    pub name2id: IndexMap<(Vec<CompactString>, Option<i32>), usize>,
     /// The flattened hash values.
     pub hashes: Vec<u64>,
 }
@@ -41,9 +98,71 @@ impl BitState {
             self.last_state != self.cur_state
         {
             // a switch happens on last_index.
-            *h = (
-                *h * 80267270009u64 + self.last_index
-            ) * 257u64 + self.cur_state as u64 + 1;
+            *h = h.wrapping_mul(80267270009u64)
+                .wrapping_add(self.last_index)
+                .wrapping_mul(257u64)
+                .wrapping_add(self.cur_state as u64 + 1);
+        }
+    }
+}
+
+/// Recursively enumerate the var definitions in VCD header.
+///
+/// If a hier temp vector is provided, the callback function
+/// will receive a hierarchy vector.
+fn enumerate_vars(
+    item: &[ScopeItem],
+    mut hier: Option<&mut Vec<CompactString>>,
+    f: &mut impl FnMut(&Var, Option<&Vec<CompactString>>)
+) {
+    for i in item {
+        if let Some(h) = hier.as_mut() {
+            if let Some(t) = match i {
+                ScopeItem::Var(var) =>
+                    Some(var.reference.as_str()),
+                ScopeItem::Scope(scope) =>
+                    Some(scope.identifier.as_str()),
+                _ => None
+            } {
+                h.push(t.into());
+            }
+        }
+        match i {
+            ScopeItem::Var(var) => f(
+                var, hier.as_ref().map(|r| &**r)),
+            ScopeItem::Scope(scope) =>
+                enumerate_vars(
+                    &scope.children[..],
+                    hier.as_mut().map(|r| &mut **r),
+                    f
+                ),
+            _ => {}
+        }
+        if let Some(h) = hier.as_mut() {
+            if let ScopeItem::Var(_) | ScopeItem::Scope(_) = i {
+                h.pop();
+            }
+        }
+    }
+}
+
+#[inline]
+fn enumerate_bits(index: Option<ReferenceIndex>, f: &mut impl FnMut(Option<i32>, usize)) {
+    use ReferenceIndex::*;
+    match index {
+        None => f(None, 0),
+        Some(BitSelect(idx)) => f(Some(idx), 0),
+        Some(Range(msb, lsb)) => {
+            if msb > lsb {
+                for (offset, i) in (lsb..(msb + 1)).rev().enumerate() {
+                    f(Some(i), offset);
+                }
+            }
+            else {
+                for (offset, i) in (msb..(lsb + 1)).enumerate() {
+                    f(Some(i), offset);
+                }
+            }
         }
     }
 }
@@ -60,7 +179,7 @@ impl HashDB {
 
     /// Internal helper function:
     /// build metadata from a VCD file header, including a
-    /// mapping between the IdCode and hash positions.
+    /// mapping between the IdCode and hash start positions.
     ///
     /// This will insert missing signals.
     /// If previous equaled signals are no longer equal,
@@ -76,96 +195,70 @@ impl HashDB {
         let header = parser.parse_header()?;
         
         // then we build metadata (id to hash index).
+        // we first calculate the number of idcodes.
         let old_num_names = self.name2id.len();
-        use vcd_ng::{ ScopeItem, Var };
-        fn enumerate_bits(
-            item: &[ScopeItem],
-            mut hier: Option<&mut Vec<CompactString>>,
-            f: &mut impl FnMut(&Var, Option<&Vec<CompactString>>)
-        ) {
-            for i in item {
-                if let Some(h) = hier.as_mut() {
-                    if let Some(t) = match i {
-                        ScopeItem::Var(var) =>
-                            Some(var.reference.as_str()),
-                        ScopeItem::Scope(scope) =>
-                            Some(scope.identifier.as_str()),
-                        _ => None
-                    } {
-                        h.push(t.into());
-                    }
-                }
-                match i {
-                    ScopeItem::Var(var) => f(
-                        var, hier.as_ref().map(|r| &**r)),
-                    ScopeItem::Scope(scope) =>
-                        enumerate_bits(
-                            &scope.children[..],
-                            hier.as_mut().map(|r| &mut **r),
-                            f
-                        ),
-                    _ => {}
-                }
-                if let Some(h) = hier.as_mut() {
-                    if match i {
-                        ScopeItem::Var(_) => true,
-                        ScopeItem::Scope(_) => true,
-                        _ => false
-                    } {
-                        h.pop();
-                    }
-                }
-            }
-        }
         let mut id_count = 0;
-        enumerate_bits(&header.items[..], None, &mut |v, _| {
+        enumerate_vars(&header.items[..], None, &mut |v, _| {
             id_count = (v.code.0 as usize + 1).max(id_count);
         });
         let mut id2hash = vec![usize::MAX; id_count];
-        // used_hashes stores the hash places that were already
-        // referenced. if two idcode refers to one hash place,
-        // we need to do manual hash cloning.
-        // start -> (idcode, width)
-        let mut used_hashes =
-            HashMap::<usize, (u64, usize)>::new();
-        enumerate_bits(
+
+        let mut hash_used = vec![u64::MAX; self.hashes.len()];
+        // we enumerate all vars, and maintain these data:
+        // 1. id2hash: we should obtain the mapping
+        //    from idcode to hash start.
+        // 2. self.name2id: we should insert any new (name, index)
+        //    tuples there.
+        // 3. hash_used: when alias is broken, we bails out.
+        //
+        // for multi-bit vectors, we have 1 entry in id2hash,
+        // but multiple entries in name2id (should have indices
+        // in the same order as multi-bit vcd signals)
+        enumerate_vars(
             &header.items[..], Some(&mut Vec::new()),
             &mut |var, hier| {
                 let hier = hier.unwrap();
-                let (start, width) = match self.name2id.get(hier) {
-                    Some(v) => *v,
+                use ReferenceIndex::*;
+                let first_bit = match var.index {
+                    None => None,
+                    Some(BitSelect(idx)) => Some(idx),
+                    Some(Range(msb, _lsb)) => Some(msb)
+                };
+
+                let start = match self.name2id.get(&(
+                    hier, first_bit
+                ) as &dyn HierNameIdx) {
+                    // name2id got the first entry.
+                    Some(id) => *id,
+                    // no entry, so we insert a new set of hashes.
                     None => {
-                        let start = self.hashes.len();
-                        self.hashes.extend((0..var.size).map(|_| 0));
-                        used_hashes.insert(start, (
-                            var.code.0, var.size as usize));
-                        (start, var.size as usize)
+                        let start = match id2hash[var.code.0 as usize] {
+                            usize::MAX => {
+                                let start = self.hashes.len();
+                                self.hashes.extend((0..var.size).map(|_| 0));
+                                hash_used.extend((0..var.size).map(|_| u64::MAX));
+                                start
+                            },
+                            idp @ _ => idp
+                        };
+                        enumerate_bits(var.index, &mut |idx, offset| {
+                            self.name2id.insert(
+                                (hier.clone(), idx),
+                                start + offset);
+                        });
+                        start
                     }
                 };
-                assert_eq!(width, var.size as usize);
+
+                // check broken alias
+                let hu = &mut hash_used[start];
+                assert!(*hu == var.code.0 || *hu == u64::MAX);
+                *hu = var.code.0;
+
+                // store into hashes
                 let idp = &mut id2hash[var.code.0 as usize];
                 assert!(*idp == start || *idp == usize::MAX);
-                use std::collections::hash_map::Entry::*;
-                match used_hashes.entry(start) {
-                    Vacant(v) => {
-                        *idp = start;
-                        v.insert((var.code.0, width));
-                    }
-                    Occupied(o) if o.get().0 != var.code.0 => {
-                        let owidth = o.get().1;
-                        assert_eq!(owidth, width);
-                        let nstart = self.hashes.len();
-                        self.hashes.extend((0..width).map(|_| 0));
-                        for i in 0..width {
-                            self.hashes[nstart + i] =
-                                self.hashes[start + i];
-                        }
-                        *idp = nstart;
-                    }
-                    _ => {
-                        *idp = start;
-                    }
-                }
+                *idp = start;
             }
         );
         if old_num_names != self.name2id.len() {
@@ -203,7 +296,6 @@ impl HashDB {
         // stream read signals and update hashes
         let f = File::open(vcd_file)?;
         let mut parser = FastFlow::new(f, 65536);
-        use vcd_ng::{ FastFlowToken, FFValueChange };
         let mut cur_time_id = 0;
         let mut states = vec![BitState {
             last_index: 0, last_state: 0, cur_state: 0
